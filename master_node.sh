f4ce76525b9203ffbcf985ba5b9c4ba6c4aeb16c
sunjin7725 <sunjin7725@gmail.com>
2023-03-08 12:27:29 +0900

    update sh

--- master_node.sh
+++ master_node.sh
@@ -1,7 +1,7 @@
 if [ $# -eq 1 ]; then
     # if you want to install specific version of k3s
     VERSION=$1
-    curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="${VERSION}" sh -s - server --cluster-init --disable traefik --docker
+    curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="${VERSION}" sh -s - server --cluster-init --disable traefik --docker --data-dir=/home/data
 else
     # if you want to install latest version of k3s
     curl -sfL https://get.k3s.io | sh -s - server --cluster-init --disable traefik --docker
